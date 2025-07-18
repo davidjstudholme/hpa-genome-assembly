66cb1f09aae17b8fd08b53da3b7bca92fb686c57
David Studholme <d.j.studholme@exeter.ac.uk>
2025-07-18 21:58:43 +0000

    circalated contig 800

--- Noks1.bacterial_contigs/commands.sh
+++ Noks1.bacterial_contigs/commands.sh
@@ -2,7 +2,7 @@ conda activate circlator_env
 conda list -n circlator_env > circlator_env_packages.txt
 conda env export > circlator_env.yaml
 
-for i in 387 609 721 799 508; do
+for i in 387 609 721 799 508 800; do
     echo contig $i
     circlator fixstart bacterial.contig_"$i".fasta  contig_"$i".circlator
 done
