f90e8c2134fb39c3c5b759014059fcddb9abb1eb
David Studholme <d.j.studholme@exeter.ac.uk>
2025-07-19 13:34:28 +0000

    Added final, cleaned version of Cala2 assembly

--- final_assemblies/commands.sh
+++ final_assemblies/commands.sh
@@ -1,6 +1,7 @@
 ### Set up BUSCO
 wget https://busco-data.ezlab.org/v5/data/lineages/stramenopiles_odb12.2025-07-01.tar.gz
 tar zxvf stramenopiles_odb12.2025-07-01.tar.gz
+
 #conda create -n busco_env -c bioconda -c conda-forge busco
 conda activate busco_env
 conda list -n busco_env > busco_env_packages.txt
@@ -23,5 +24,7 @@ busco --plot busco.all
 conda activate quast_env
 conda list -n quast_env > quast_env_packages.txt
 conda env export > quast_env.yaml
-
 quast.py *.fna *.fasta
+
+### Compress the cleaned assembly files
+gzip *.fasta
